581ff62df0d111d7da7fefb6f6262e01b8fe0c1e
Pascal <paco@LAPTOP-LMBID7V2.localdomain>
2026-01-24 00:19:21 +0100

    test: add revert and event test

--- test/Guestbook.test.ts
+++ test/Guestbook.test.ts
@@ -3,6 +3,7 @@ import {
 } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 
 describe("Guestbook", function () {
 
@@ -39,6 +40,19 @@ describe("Guestbook", function () {
             expect(entries.length).to.equal(1);
             expect(entries[0].message).to.equal(message);
             expect(entries[0].writer).to.equal(owner.address);
+        });
+
+        it("Should reject empty messages", async function () {
+            const { guestbook } = await loadFixture(deployGuestbookFixture);
+
+            await expect(guestbook.writeMessage("")).to.be.revertedWith("Nachricht darf nicht leer sein");
+        });
+
+        it("Should emit an event on new entry", async function () {
+            const { guestbook, owner } = await loadFixture(deployGuestbookFixture);
+            const message = "Hello Blockchain!";
+
+            await expect(guestbook.writeMessage(message)).to.emit(guestbook, "NewEntry").withArgs(owner.address, message, anyValue);
         });
 
     });
